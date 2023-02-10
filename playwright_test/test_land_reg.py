1abe7502acfcb01fb2f6f6a41b15e73f25ae45a3
Malachy Egbuna <zubbyik@gmail.com>
2023-02-10 17:48:34 +0100

    land_use_reg.py update

--- playwright_test/test_land_reg.py
+++ playwright_test/test_land_reg.py
@@ -15,16 +15,16 @@ def test_land_use_act_and_scrape_the_text(page: Page):
     for row in range(0, dataframe1.max_row):
         for col in dataframe1.iter_cols(1, dataframe1.max_column):
             data.append(col[row].value)
-    print(data)
+
     # Get to the search Page
     # page.goto("https://find-and-update.company-information.service.gov.uk/")
     # page.get_by_role("button", name="Reject analytics cookies").click()
     # page.get_by_role("button", name="Hide this message").click()
-    # page.get_by_placeholder("Start here...").click()
-    # page.get_by_placeholder("Start here...").fill("ererer")
-    # page.get_by_role("button", name="Search").click()
-    # heading = page.get_by_role("heading", name="No results found").text_content()
+
     # print("\n"+heading)
     # page.close()
-
-    print('')
+    for item in data:
+        page.get_by_placeholder("Start here...").click()
+        page.get_by_placeholder("Start here...").fill(item)
+        page.get_by_role("button", name="Search").click()
+        heading = page.get_by_text("OE005420 - Registered on 28 November 2022").text_content()
