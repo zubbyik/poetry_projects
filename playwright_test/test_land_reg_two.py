3ee2f6b61a42487806dff76b6484cee2506fe0c7
zubby--samsung <zubbyik@gmail.com>
2023-02-15 08:31:56 +0100

    test land two update launch.json for vscode

--- playwright_test/test_land_reg_two.py
+++ playwright_test/test_land_reg_two.py
@@ -1,16 +1,18 @@
-from playwright.sync_api import Page, expect
+from playwright.async_api import Page, expect
 import re
 import pathlib
 
 reg = ["ELDRITCH LIMITED",
-"QUEENTON LIMITED",
-"DUNBAR VENTURES CORP.",
-"GESTRUST SA",
-"BARMOUTH LIMITED",
-"DREEMGARROW LIMITED",
-"KAA CONSULTING S.A. BVI"]
+       "QUEENTON LIMITED",
+       "DUNBAR VENTURES CORP.",
+       "GESTRUST SA",
+       "BARMOUTH LIMITED",
+       "DREEMGARROW LIMITED",
+       "KAA CONSULTING S.A. BVI"]
 
 url = 'https://find-and-update.company-information.service.gov.uk/'
+
+
 def test_land_reg_two(page: Page):
     page.goto(url)
     page.get_by_text("Reject analytics cookies").click()
@@ -20,10 +22,17 @@ def test_land_reg_two(page: Page):
             search_box.fill(item)
             page.get_by_role("button", name="Search").click()
             search_link = page.get_by_role("link", name=item[:8]).first
-            search_link.click()
-            title = page.title()
-            print(title)
-        except NameError or TimeoutError:
-            print('not found')
-            page.go_back()
+            search_content = search_link.text_content()
+            if item[:8] in search_content().strip():
+                search_link.click()
+                title = page.title()
+                print(title)
+                page.goto(url)
+            else:
+                page.goto(url)
+
+        except (NameError, TimeoutError) as exception:
+            print(f"{exception} found")
+            page.goto(url)
     page.close()
+
