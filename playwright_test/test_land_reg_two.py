188d5d79812a9cc081cd59857a30785a8ca81743
zubby--samsung <zubbyik@gmail.com>
2023-02-16 05:22:42 +0100

    test land two update launch.json for vscode

--- playwright_test/test_land_reg_two.py
+++ playwright_test/test_land_reg_two.py
@@ -3,18 +3,25 @@ from pathlib import Path
 import openpyxl
 import re
 
-
+# data has to be a set so that every data duplicate will be taken care of
 data = set()
 company_numbers = []
+# Reading the Excel file with openpyxl
+# ========================================
 dataframe = openpyxl.load_workbook(Path().joinpath('land_use_reg.xlsx'))
 dataframe1 = dataframe.active
+# =============== Ignore these ===================
 # cell = dataframe1.cell(row=2, column=1)
 # print('hello')
 # print("result: \n"+cell.value)
+# ================================================
+#  Looping through the result from reading the Excel file
+# =======================================================
 for row in range(0, dataframe1.max_row):
     for col in dataframe1.iter_cols(1, dataframe1.max_column):
         data.add(col[row].value)
 
+# ======= Ignore this Test Data ======= #
 # reg = ["ELDRITCH LIMITED",
 # "QUEENTON LIMITED",
 # "DUNBAR VENTURES CORP.",
@@ -22,8 +29,11 @@ for row in range(0, dataframe1.max_row):
 # "BARMOUTH LIMITED",
 # "DREEMGARROW LIMITED",
 # "KAA CONSULTING S.A. BVI"]
+# ========================================
 
 url = 'https://find-and-update.company-information.service.gov.uk/'
+
+
 def test_land_reg_two(page: Page):
     page.goto(url)
     page.get_by_text("Reject analytics cookies").click()
@@ -35,10 +45,11 @@ def test_land_reg_two(page: Page):
             search_link = page.get_by_role("link", name=item[:8]).first
             result = page.get_by_text(item[:8]).first
             not_found = page.get_by_text("No results found")
-            if(search_link.is_visible() or search_link.text_content() != "" or not_found or result.is_visible()):
+            if (search_link.is_visible() or search_link.text_content() != "" or not_found or result.is_visible()):
                 search_link.click()
-                comp_number = page.locator("#company-number").get_by_text(re.compile("^OE\d{6}")).text_content()
-                if(comp_number != ''):
+                comp_number = page.locator(
+                    "#company-number").get_by_text(re.compile("^OE\d{6}")).text_content()
+                if (comp_number != ''):
                     company_numbers.append(comp_number)
                 page.goto(url)
             else:
@@ -46,5 +57,11 @@ def test_land_reg_two(page: Page):
         except Exception as e:
             print("Link not found due to " + e.__str__())
             page.goto(url)
-    print(company_numbers)
+    with open('companies.txt', 'w') as write_companies:
+        for item in company_numbers:
+            if (comp_number[:-1]):
+                write_companies.write(item)
+            else:
+                write_companies.write(item+", ")
+    print(write_companies)
     page.close()
